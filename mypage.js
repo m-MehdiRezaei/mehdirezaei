f5d708878f0645d3a8f6784d994fefe2759680d1
m-MehdiRezaei <154101981+m-MehdiRezaei@users.noreply.github.com>
2023-12-17 18:44:18 -0700

    Add files via upload

---
+++ mypage.js
@@ -0,0 +1,12 @@
+function showHTML() {
+    window.location.href = 'index.html'; // Redirect to the HTML cheatsheet
+}
+
+function showCSS() {
+    window.location.href = 'css-cheatsheet.html'; // Redirect to the CSS cheatsheet
+}
+
+function showMypage() {
+    window.location.href = 'myPage.html'; // Redirect to the JS cheatsheet
+}
+
---
+++ script.js
@@ -0,0 +1,35 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Get all elements with class "code-explanation"
+    var codeExplanations = document.getElementsByClassName('code-explanation');
+
+    // Initially hide all code explanations
+    for (var i = 0; i < codeExplanations.length; i++) {
+        codeExplanations[i].style.display = "none";
+    }
+});
+
+function toggleSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    var codes = section.getElementsByClassName('code-explanation');
+
+    for (var i = 0; i < codes.length; i++) {
+        if (codes[i].style.display === "none" || codes[i].style.display === "") {
+            codes[i].style.display = "block";
+        } else {
+            codes[i].style.display = "none";
+        }
+    }
+}
+
+function showHTML() {
+    window.location.href = 'index.html'; // Redirect to the HTML cheatsheet
+}
+
+function showCSS() {
+    window.location.href = 'css-cheatsheet.html'; // Redirect to the CSS cheatsheet
+}
+
+function showMypage() {
+    window.location.href = 'myPage.html'; // Redirect to the JS cheatsheet
+}
